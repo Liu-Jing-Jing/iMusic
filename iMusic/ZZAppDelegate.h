1ceba8b660e1fcfb9fb7410ff85f8fc55c910bc4
Mark <marklewis@qq.com>
2019-03-01 16:47:23 +0800

    Initial Commit

---
+++ iMusic/ZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZZAppDelegate.h
+//  iMusic
+//
+//  Created by Mark Lewis on 19-3-1.
+//  Copyright (c) 2019年 Tsinghua University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iMusic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iMusic
+//
+//  Created by Mark Lewis on 19-3-1.
+//  Copyright (c) 2019年 Tsinghua University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZZAppDelegate class]));
+    }
+}
